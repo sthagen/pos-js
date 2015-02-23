3e7f6e64bec6ea2decfebf9d52efd729d8e6e65d
Mark Birbeck <mark.birbeck@gmail.com>
2015-02-23 02:18:02 +0000

    [#6] add failing test for hash

--- test/lexer.js
+++ test/lexer.js
@@ -19,4 +19,16 @@ describe('Lexer#lex', function(){
       .lex("I made $5.42 today (but it should have been more).")
       .should.eql(['I', 'made', '$', '5.42', 'today', '(', 'but', 'it', 'should', 'have', 'been', 'more', ')', '.']);
   });
+
+  it('should tokenize hash', function(){
+    lexer
+      .lex("I'm fixing issue #6.")
+      .should.eql(['I', '\'', 'm', 'fixing', 'issue', '#', '6', '.']);
+  });
+
+  it('should tokenize hash when not followed by a number', function(){
+    lexer
+      .lex("I'm fixing issue #tags.")
+      .should.eql(['I', '\'', 'm', 'fixing', 'issue', '#', 'tags', '.']);
+  });
 });
